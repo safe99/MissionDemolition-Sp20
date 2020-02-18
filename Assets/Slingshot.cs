345d200e593de990fd90cf0717c9c14d69f8f868
safe99 <55143184+safe99@users.noreply.github.com>
2020-02-17 22:20:51 -0500

    Slingshot Script
    It can sling balls

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 {
     public GameObject prefabProjectile;
+    public float velocityMulti = 8f;
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aiming;
+    private Rigidbody projectileRigidbody;
 
     private void Awake()
     {
@@ -18,12 +20,42 @@ public class Slingshot : MonoBehaviour
         launchPos = launchPointTrans.position;
     }
 
+    void Update()
+    {
+        if (!aiming) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if(mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aiming = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMulti;
+            projectile = null;
+        }
+    }
+
     private void OnMouseDown()
     {
         aiming = true;
         projectile = Instantiate(prefabProjectile) as GameObject;
         projectile.transform.position = launchPos;
-        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+
     }
 
     // Start is called before the first frame update
