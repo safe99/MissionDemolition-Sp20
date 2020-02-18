2faf693024b2a4ffc7d8d2c5d5821349d8f2451b
safe99 <55143184+safe99@users.noreply.github.com>
2020-02-17 22:38:25 -0500

    More Camera Scripting

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -6,6 +6,9 @@ public class FollowCam : MonoBehaviour
 {
 
     static public GameObject POI;
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -19,8 +22,14 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
     // Start is called before the first frame update
