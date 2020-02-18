0d5225b772fb02ae9c30efbe33072ea8502b0def
safe99 <55143184+safe99@users.noreply.github.com>
2020-02-17 21:53:08 -0500

    Slingshot + SlingshotScripts

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject prefabProjectile;
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aiming;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    private void OnMouseDown()
+    {
+        aiming = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Start is called before the first frame update
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+}
